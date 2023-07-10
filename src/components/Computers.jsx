557f0b3f2755c2ec0184d9d5272215afeb35713d
A1exsandro <alexsandro.acp@gmail.com>
2023-07-10 09:06:13 -0400

    feat: add component computers

---
+++ src/components/Computers.jsx
@@ -0,0 +1,57 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { useIsMobile } from '../contexts/IsMobileContext'
+import CanvasLoader from './Loader'
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]} 
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 1 : 2}
+        position={isMobile ? [0, -1, 0] : [0, -3, 0]}
+        rotation={[0, 0, 0]}
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  const { isMobile } = useIsMobile()
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile}/>
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
