7828986e1c2ddfb5b33c507093a01cfeecb81db2
A1exsandro <alexsandro.acp@gmail.com>
2023-07-10 15:57:56 -0400

    feat: add cardJsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,17 @@
 import React from 'react' 
+import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 import { SectionWrapper } from '../hoc'
+import { useIsMobile } from '../contexts/IsMobileContext'
 
 const ServiceCard = ({ index, title, icon }) => {
-  return (
+  const { isMobile } = useIsMobile()
+
+  const cardJsx = (
     <motion.div
       variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -27,6 +31,16 @@ const ServiceCard = ({ index, title, icon }) => {
         </h3>
       </div>
     </motion.div>
+  )
+
+  return (
+    isMobile ? (
+      cardJsx
+    ) : (
+      <Tilt className="xs:w-[250px] w-full">
+        { cardJsx }
+      </Tilt>
+    )
   )
 }
 
